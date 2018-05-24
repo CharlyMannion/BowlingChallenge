f6b7f67ce6d41545d06ca97e6ed725082b350d16
Charly Mannion <charlottemannion100@gmail.com>
2018-05-24 20:04:11 +0100

    perfect game test written

--- jasmine-standalone-3.1.0/spec/gameSpec.js
+++ jasmine-standalone-3.1.0/spec/gameSpec.js
@@ -20,6 +20,13 @@ describe("Game", function(){
       game.scoreCard = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
       expect(game.score()).toEqual(20);
     });
+    it("should return 300 if the player rolls 12 strikes", function (){
+      var times = 12;
+      for(var i=0; i < times; i++){
+        game.bowlBall(10)
+      }
+      expect(game.score()).toEqual(300)
+    });
   });
 
   describe("bowling balls with basic scores", function(){
@@ -34,7 +41,10 @@ describe("Game", function(){
         game.bowlBall(2)
       }
       expect(game.scoreCard).toEqual([2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2])
-    })
+    });
+    // it("should not allow a 21st role of the ball if the 19th ball is a basic score", )
+    // it("should end the game after 20 roles of the ball for a basic scoring game", )
+    // it("should give the game score after 20 roles of a basic scoring game", )
   });
 
 });
